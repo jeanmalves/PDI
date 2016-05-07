a7c0bd253426c06a6263fcc298762d181bcbc7eb
brunoroberto <bfernandes@alunos.utfpr.edu.br>
2016-05-07 08:41:50 -0300

    Final version of chroma key, now is working!

--- chromakey/main.c
+++ chromakey/main.c
@@ -7,8 +7,8 @@
 #include "pdi.h"
 
 /*============================================================================*/
-#define INPUT_IMAGE "3.bmp"
-#define INPUT_IMAGE_BACKGROUND "jardim_botanico.bmp"
+#define INPUT_IMAGE "2.bmp"
+#define INPUT_IMAGE_BACKGROUND "paisagem.bmp"
 
 #define NEGATIVO 0
 #define THRESHOLD 0.7f
@@ -16,63 +16,97 @@
 #define LARGURA_MIN 10
 #define N_PIXELS_MIN 20
 
-/*============================================================================*/
+#define TRUE 1
+#define FALSE 0
 
-void binariza1 (Imagem* in, Imagem* out, float threshold);
-int main() {
+void chroma_key(Imagem* in, Imagem* bg, Imagem* out, float d)
+{
+    int row, column;
+    for (row = 0; row < in->altura; row++)
+    {
+        for (column = 0; column < in->largura; column++)
+        {
+            float green = in->dados[1][row][column];
+            float red = in->dados[0][row][column];
+            float blue = in->dados[2][row][column];
 
-  // Abre a imagem em escala de cinza, e mantem uma copia colorida dela para desenhar a saida.
-  Imagem* img = abreImagem (INPUT_IMAGE, 3);
-  if (!img)
-  {
-      printf ("Erro abrindo a imagem.\n");
-      exit (1);
-  }
+            printf("linha = %d - coluna %d - red = %f - green = %f - blue = %f\n", row, column, red, green, blue);
 
-  Imagem* img_back = abreImagem (INPUT_IMAGE_BACKGROUND, 3);
-  if (!img_back)
-  {
-      printf ("Erro abrindo a imagem a ser aplicada no chromakey.\n");
-      exit (1);
-  }
-  Imagem* img_back_out = criaImagem (img->largura, img->altura, 3);
-
-  redimensionaNN (img_back, img_back_out);
-  salvaImagem (img_back_out, "back_redimencionada.bmp");
-
-//  Imagem* img_out = criaImagem (img->largura, img->altura, 1);
-
-
-  //binariza (img, img_out, THRESHOLD);
- //binariza1(img, img_out, THRESHOLD);
-//  salvaImagem (img_out, "01 - binarizada.bmp");
-
-  destroiImagem (img);
-  destroiImagem (img_back);
-
-  return 0;
+            int eh_fundo = FALSE;
+            
+            // verifica se eh fundo
+            if ((green > (red + d)) && (green > (blue + d)))
+            {
+                eh_fundo = TRUE;
+            }
+            
+            int canal;
+            for (canal = 0; canal < in->n_canais; canal++)
+            {
+                if (eh_fundo == TRUE)
+                {
+                    out->dados[canal][row][column] = bg->dados[canal][row][column];
+                }
+                else
+                {
+                    out->dados[canal][row][column] = in->dados[canal][row][column];
+                }
+            }
+            
+        }
+    }
 }
 
-void binariza1 (Imagem* in, Imagem* out, float threshold)
+/*============================================================================*/
+int main(int argc, char **argv)
 {
-    if (in->largura != out->largura || in->altura != out->altura || in->n_canais != out->n_canais)
+    Imagem* img = abreImagem (INPUT_IMAGE, 3);
+    if (!img)
     {
-        printf ("ERRO: binariza: as imagens precisam ter o mesmo tamanho e numero de canais.\n");
-        exit (1);
+        printf("Erro abrindo a imagem.\n");
+        exit(1);
     }
 
-    float limiar_red = in->dados[0][0][0];
-    float limiar_green = in->dados[1][0][0];
-    float limiar_blue = in->dados[2][0][0];
-    int threshold1;
-    int channel, row, col, limiar;
-    //for (channel = 0; channel < in->n_canais; channel++){
-        for (row = 0; row < in->altura; row++){
-            for (col = 0; col < in->largura; col++) {
-
-
-
-            }
+    Imagem* img_bg = abreImagem (INPUT_IMAGE_BACKGROUND, 3);
+    if (!img_bg)
+    {
+        printf("Erro abrindo a imagem a ser aplicada no chromakey.\n");
+        exit(1);
+    }
+    
+    Imagem *img_bg_redimensionada = NULL;
+    if (img_bg->altura != img->altura || img_bg->largura != img->largura)
+    {
+        img_bg_redimensionada = criaImagem(img->largura, img->altura, img->n_canais);
+        if (!img_bg_redimensionada)
+        {
+            printf("Erro ao criar a imagem para redimensionar.\n");
+            exit(1);
         }
-    //}
+        redimensionaNN(img_bg, img_bg_redimensionada);
+    }
+    
+    if (img_bg_redimensionada == NULL)
+    {
+        img_bg_redimensionada = img_bg;
+    }
+    
+    Imagem *img_final = criaImagem(img->largura, img->altura, img->n_canais);
+    if (!img_final)
+    {
+        printf("Erro ao criar imagem para armazenar o resultado.\n");
+        exit(1);
+    }
+    
+    chroma_key(img, img_bg_redimensionada, img_final, 0.1);
+    
+    salvaImagem(img_final, "chroma_key.bmp");
+    
+    destroiImagem(img_final);
+    destroiImagem(img_bg_redimensionada);
+    destroiImagem(img_bg);
+    destroiImagem(img);
+    
+    return 0;
 }
+
