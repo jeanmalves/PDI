2153bfb4da411177cffa2bdfb27fd98441a21295
Jeam Alves <jeanmalves@gmail.com>
2016-05-06 21:29:10 -0300

    add função redimenciona

--- chromakey/imagem.h
+++ chromakey/imagem.h
@@ -1,12 +1,12 @@
 /*============================================================================*/
-/* MANIPULAÇÃO DE ARQUIVOS BMP                                                */
+/* MANIPULAï¿½ï¿½O DE ARQUIVOS BMP                                                */
 /*----------------------------------------------------------------------------*/
 /* Autor: Bogdan T. Nassu - nassu@dainf.ct.utfpr.edu.br                       */
 /*============================================================================*/
-/** Este arquivo traz declarações de um tipo e rotinas para manipulação de
- * arquivos bmp. Como temos um propósito puramente didático, apenas um sub-
- * conjunto mínimo do formato foi implementado. Usamos aqui matrizes alocadas
- * dinamicamente e números de ponto flutuante. Esta não é a forma mais eficiente
+/** Este arquivo traz declaraï¿½ï¿½es de um tipo e rotinas para manipulaï¿½ï¿½o de
+ * arquivos bmp. Como temos um propï¿½sito puramente didï¿½tico, apenas um sub-
+ * conjunto mï¿½nimo do formato foi implementado. Usamos aqui matrizes alocadas
+ * dinamicamente e nï¿½meros de ponto flutuante. Esta nï¿½o ï¿½ a forma mais eficiente
  * de se trabalhar com imagens, mas procuramos priorizar a clareza e a
  * facilidade de uso. */
 /*============================================================================*/
@@ -21,13 +21,13 @@ typedef struct
 	int largura;
 	int altura;
 	int n_canais;
-	float*** dados; /* Uma matriz de dados por canal. Acessar com 3 índices: [canal][y][x]. */
+	float*** dados; /* Uma matriz de dados por canal. Acessar com 3 ï¿½ndices: [canal][y][x]. */
 } Imagem;
 
 /*----------------------------------------------------------------------------*/
-/* Por simplicidade e compatibilidade, nós sempre consideramos a leitura e
- * escrita de imagens com 3 canais, 24bpp. Todas as conversões para escala de
- * cinza e float são feitas internamente. */
+/* Por simplicidade e compatibilidade, nï¿½s sempre consideramos a leitura e
+ * escrita de imagens com 3 canais, 24bpp. Todas as conversï¿½es para escala de
+ * cinza e float sï¿½o feitas internamente. */
 
 Imagem* criaImagem (int largura, int altura, int n_canais);
 void destroiImagem (Imagem* img);
@@ -35,6 +35,8 @@ Imagem* abreImagem (char* arquivo, int n_canais);
 int salvaImagem (Imagem* img, char* arquivo);
 Imagem* clonaImagem (Imagem* img);
 void copiaConteudo (Imagem* in, Imagem* out);
+void redimensionaNN (Imagem* in, Imagem* out);
+void redimensionaBilinear (Imagem* in, Imagem* out);
 
 /*============================================================================*/
 #endif /* __IMAGEM_H */
